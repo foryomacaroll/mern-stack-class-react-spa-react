b87e79ff30ae64d38706199088d241843fa7061a
foryomacaroll <foryoma.caroll@gmail.com>
2024-09-13 23:18:17 +0700

    ep44-final_assets-use-from-app_first-way_second-way_/assets/imgimg.png

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
+import Image from "./assets/imgimg.png";
 
 function App() {
   let name = "Hlaing Min Than";
-  return <h1>Hello {name}</h1>;
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      {/* first way */}
+      {/* <img src="/imgimg.png" alt=""></img> */}
+      {/* second way */}
+      <img src={Image}></img>
+    </div>
+  );
 }
 
 export default App;
